7a8fea17d74965095ea0b3d04c9c7307be2f752d
Dhiovana <dhiovana.moreira@aluno.cefet-rj.br>
2023-06-06 14:43:21 -0300

    Pegando as informações do usuário

--- main.cpp
+++ main.cpp
@@ -1,3 +1,27 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
+
+
+int calculo(int num1, int num2, int calcOperador, int &aux){
+	int resultado;
+	switch(calcOperador){
+	case '+':
+	resultado = num1 + num2;
+	break;
+	case '-':
+	resultado = num1 - num2;
+	break;
+	case '*':
+	resultado = num1 * num2;
+	break;
+	case '/':
+	resultado = num1 / num2;
+	break;
+	default:
+	aux = 0;
+	return 0;
+    }
+    aux = 1;
+	return resultado;
+}
